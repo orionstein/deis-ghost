bb689baa2814b6e6e11ea5a2213f936c062719d7
orion <orion@vagrant.home>
2015-06-10 10:38:36 -0700

    Deis urlurl

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ config = {
     // When running Ghost in the wild, use the production environment.
     // Configure your URL and mail settings here
     production: {
-        url: 'https://www.notsafeforproduction.com',
+        url: process.env.GHOST_URL,
         mail: {},
         database: {
           client: 'postgres',
